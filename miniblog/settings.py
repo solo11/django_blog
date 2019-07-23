beb037756e089d9d6fae6a37e9776e6847c7d105
Solomon <solomonraj11@gmail.com>
2019-07-23 18:28:49 +0530

    HTML templates for the site

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4kqn-%n**vfx5ib)qru4ot14^#bg(wil$^30@eb^61rk2)=z%2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','solo11.pythonanywhere.com']
 
 
 # Application definition
